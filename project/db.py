6b61f90582b76c79e7b93638aff4a51169a10ef7
Alexandre de Freitas Souza <alexandrefsoficial@gmail.com>
2023-06-25 19:43:28 -0300

    feat: initial classes added to database schema

--- project/db.py
+++ project/db.py
@@ -12,7 +12,17 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(100))
     authenticated = db.Column(db.Boolean, default=False)
 
-class UserAdmin(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    userId = db.Column(db.Integer, ForeignKey('user.id'))
+class UserAdmin(db.Model):
+    id = db.Column(db.Integer, ForeignKey('user.id', ondelete='CASCADE'))
     user = relationship('User', backref='admin')
+
+class Movies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    imageUrl = db.Column(db.String(200))
+
+class UserPreferences(db.Model):
+    userId = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), primary_key=True)
+    user = relationship('User', backref='preferences')
+    movieId = db.Column(db.Integer, db.ForeignKey('movies.id', ondelete='CASCADE'), primary_key=True)
+    movie = relationship('Movies', backref='preferences')
