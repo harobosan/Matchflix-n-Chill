672b0312b3e3ab61b31fee32871ed8090cd736d9
Alexandre de Freitas Souza <alexandrefsoficial@gmail.com>
2023-06-25 19:23:01 -0300

    Feat: Class UserAdmin added to database

--- project/db.py
+++ project/db.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -9,3 +11,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(1000))
     password = db.Column(db.String(100))
     authenticated = db.Column(db.Boolean, default=False)
+
+class UserAdmin(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, ForeignKey('user.id'))
+    user = relationship('User', backref='admin')
