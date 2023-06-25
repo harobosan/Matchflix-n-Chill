d640a98cd64fdc541d0698c9f5b3d230ccb79942
Alexandre de Freitas Souza <alexandrefsoficial@gmail.com>
2023-06-25 20:46:11 -0300

    fix: user_authenticates lint fixed

--- project/user.py
+++ project/user.py
@@ -1,7 +1,7 @@
 from werkzeug.security import check_password_hash
 from .db import db, User
 
-def userAuthenticates(email, password):
+def user_authenticates(email, password):
     user = User.query.filter_by(email=email).first()
     if not user or not check_password_hash(user.password, password):
         return False
