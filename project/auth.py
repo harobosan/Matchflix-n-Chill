c6cfda716e6579cfe136d82c0b1eb4821c59ce59
Gabriel Halabi <gabriel.n.halabi@gmail.com>
2023-06-25 22:30:56 -0300

    auth.py fixes

--- project/auth.py
+++ project/auth.py
@@ -1,8 +1,6 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import current_user, login_user, logout_user
-from .db import db, User
-from .user import user_authenticates
+from .user import create_user, authenticate_user, disconnect_user
 
 auth = Blueprint('auth', __name__)
 
@@ -11,18 +9,18 @@ def login():
     if current_user.is_authenticated:
         flash('You are already authenticated, logout first to access this page.')
         return redirect(url_for('main.profile'))
+
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-        remember = True if request.form.get('remember') else False
-        
-        if user_authenticates(email, password):
+        remember = bool(request.form.get('remember'))
+        user = authenticate_user(email, password)
+
+        if not user:
             flash('Please check your login details and try again.')
             return redirect(url_for('auth.login'))
 
         login_user(user, remember=remember)
-        current_user.authenticated = True
-        db.session.commit()
         return redirect(url_for('main.profile'))
 
     return render_template('login.html')
@@ -32,17 +30,17 @@ def signup():
     if current_user.is_authenticated:
         flash('You are already authenticated, logout first to access this page.')
         return redirect(url_for('main.profile'))
+
     if request.method == 'POST':
         email = request.form.get('email')
         username = request.form.get('username')
         password = request.form.get('password')
+        user = create_user(email, username, password)
 
-        if User.query.filter_by(email=email).first():
+        if not user:
             flash('Email address already exists')
             return redirect(url_for('auth.signup'))
 
-        db.session.add(User(email=email, username=username, password=generate_password_hash(password, method='sha256'), authenticated=False))
-        db.session.commit()
         return redirect(url_for('auth.login'))
 
     return render_template('signup.html')
@@ -52,7 +50,7 @@ def logout():
     if not current_user.is_authenticated:
         flash('Please, authenticate to access this page.')
         return redirect(url_for('auth.login'))
-    current_user.authenticated = False
-    db.session.commit()
+
+    disconnect_user(current_user)
     logout_user()
     return redirect(url_for('main.index'))
