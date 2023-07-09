4bfd0fd1bf95a505ed95a81698b622945e5b62d4
Alexandre de Freitas Souza <alexandrefsoficial@gmail.com>
2023-07-09 15:01:35 -0300

    test: get movies list test added

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,12 +1,13 @@
 from project.db import Movie
-from project.movie import create_movie, get_all_movies
+from project.movie import create_movie, get_all_movies, get_movie_list
 from tests.factories.movies.clean_movies import clean_movies
 
 
 def test_create_movie(app):
+    clean_movies()
     create_movie('name','url')
     movie = Movie.query.filter_by(name='name').first()
-    assert movie.image_url == 'url' and movie.name == 'name'
+    assert movie.id == 1 and movie.image_url == 'url' and movie.name == 'name'
 
 def test_get_all_movies(app):
     clean_movies()
@@ -14,8 +15,8 @@ def test_get_all_movies(app):
     create_movie('name2','url2')
     create_movie('name3','url3')
     movies = get_all_movies()
-    print(movies)
-    firstCondition = (movies[0].image_url == 'url1' and movies[0].name == 'name1')
-    secondCondition = (movies[1].image_url == 'url2' and movies[1].name == 'name2')
-    thirdCondition = (movies[2].image_url == 'url3' and movies[2].name == 'name3') 
-    assert firstCondition and secondCondition and thirdCondition
+    assert movies[0].id == 1 and movies[1].id == 2 and movies[2].id == 3
+
+def test_get_movie_list(app):
+    movies = get_movie_list([2,3])
+    assert movies[0].id == 2 and movies[1].id == 3
