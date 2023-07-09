3655f59a244e9334ff0c3fd62c1290a4774b0ecd
Alexandre de Freitas Souza <alexandrefsoficial@gmail.com>
2023-07-09 15:27:32 -0300

    test: seed random movies test implemented

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -1,5 +1,5 @@
 from project.db import Movie
-from project.movie import create_movie, delete_movie, get_all_movies, get_movie_list
+from project.movie import create_movie, delete_movie, get_all_movies, get_movie_list, seed_movies
 from tests.factories.movies.clean_movies import clean_movies
 
 
@@ -8,6 +8,11 @@ def test_create_movie(app):
     create_movie('name','url')
     movie = Movie.query.filter_by(name='name').first()
     assert movie.id == 1 and movie.image_url == 'url' and movie.name == 'name'
+
+def test_create_existing_movie(app):
+    result = create_movie('name','url')
+    assert result == None
+    
 
 def test_get_all_movies(app):
     clean_movies()
@@ -28,3 +33,17 @@ def test_delete_movie(app):
     for i in movies:
         moviesNames.append(i.name)
     assert 'name2' not in moviesNames
+
+def test_seed_movies(app):
+    clean_movies()
+    movies = get_all_movies()
+    length1 = len(movies)
+    seed_movies()
+    movies2 = get_all_movies()
+    length2 = len(movies2)
+    assert length2 == length1+10
+
+def test_seed_random_movies(app):
+    seed_movies()
+    movies = get_all_movies()
+    assert len(movies) == 20
